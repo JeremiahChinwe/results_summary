a55a2439da6e3f8002665ec4819e2be1634b564b
Jeremiah Chinwe <chinwejeremiah9@gmail.com>
2023-06-30 17:15:25 +0100

    Updated the app the second time

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
-import data from "../../data.json"
+
 
 export default function Results() {
+    const data = [
+        {
+          category: "Reaction",
+          score: 80,
+          icon: "./assets/images/icon-reaction.svg"
+        },
+        {
+          category: "Memory",
+          score: 92,
+          icon: "./assets/images/icon-memory.svg"
+        },
+        {
+          category: "Verbal",
+          score: 61,
+          icon: "./assets/images/icon-verbal.svg"
+        },
+        {
+          category: "Visual",
+          score: 72,
+          icon: "./assets/images/icon-visual.svg"
+        }
+      ]
+
     return (
         <div className="container">
             <div className="page">
